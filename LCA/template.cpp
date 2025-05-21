e1712e929cd7afd791268df7bbf27f1af21fa6e0
Vinniellf <a20213736@pucp.edu.pe>
2025-05-21 18:39:39 -0500

    nueva laptop

---
+++ LCA/template.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define cases ll T=0;cin>>T;while(T--)
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define pb push_back
+#define fill(x,v) memset(x,v,sizeof(x))
+#define all(v) (v).begin(),(v).end()
+#define PI 3.14159265359
+
+typedef long double ld;
+typedef long long ll;
+
+const ll MOD = 998244353LL;
+const int INF = 1e9;
+
+solve() {
+    return;
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+    int cases; cin >> cases;
+    while (cases--) {
+        solve();
+    }
+
+    return 0;
+}
